513afd20cea4011bd33946b6146f2bd6bfe519fa
Eric Ross <epr@evross.com>
2019-07-15 19:54:56 -0700

    Added morgan

--- app/express/app.js
+++ app/express/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express()
 
+app.use(morgan('combined'));
 app.get('/', function(req, res){
     console.log("requested '/'")
     res.end('Hello World')
@@ -12,4 +15,6 @@ app.get('/', function(req, res){
 app.listen(3000, function(){
     console.log('Listening on port ' + chalk.green('3000'));
     console.log(`listening on port ${chalk.green('3000')}`);
+    debug(`listening on port ${chalk.green('3000')}`);
+    
 });
