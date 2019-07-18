3449fd357ebc11d7e341426b64b09ae9f66c0780
Eric Ross <epr@evross.com>
2019-07-17 21:45:52 -0700

    eslint and pug template

--- app/express/app.js
+++ app/express/app.js
@@ -7,15 +7,21 @@ var path = require('path');
 var app = express()
 
 app.use(morgan('tiny'));
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')));
+app.set('views','./src/views');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res){
     console.log("requested '/'")
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index',{
+        title: 'MyLibrary',
+        list: ['a','b','c']
+    });
 });
 
 app.listen(3000, function(){
